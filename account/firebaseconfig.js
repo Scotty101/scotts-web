9c530cb19bd4e8467b19576b5dbbf92eb34fa1cd
Scotty101 <scott@Antons-MacBook-Air.local>
2020-05-17 12:14:44 +0200

    Added auth

---
+++ account/firebaseconfig.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDT5U923Rgm8_1es7LMeY9d5QVrq14M8WM",
+    authDomain: "scotts-web.firebaseapp.com",
+    databaseURL: "https://scotts-web.firebaseio.com",
+    projectId: "scotts-web",
+    storageBucket: "scotts-web.appspot.com",
+    messagingSenderId: "125467102075",
+    appId: "1:125467102075:web:2f1557e18575df33b9cbde",
+    measurementId: "G-FH7WXJVQ2C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
