edc6781eb99bf8770f13fe162542c204d6e4354c
Scotty101 <scott@Antons-MacBook-Air.local>
2020-06-07 18:05:47 +0200

    Update sketch.js

--- bob/sketch.js
+++ bob/sketch.js
@@ -1,27 +1,42 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("cyan");
 }
 
 function draw() {
-  background("cyan");
-  fill(168, 98, 45);
-  // head
-  ellipse(250, 200, 820, 820);
-  line(100, 60, 300, 60); //eyebrow1
-  line(400, 60, 600, 60); //eyebrow2
+  drawHead();
+  drawEyebrow(100);
+  drawEyebrow(400);
+  drawEye(200);
+  drawEye(500);
+  drawSunglasses();
+  drawMouth();
+  drawNose();
+}
 
-  fill('white');
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawHead() {
+  fill(168, 98, 45);
+  ellipse(250, 200, 820, 820);
+}
+
+function drawEyebrow(x) {
+  line(x, 60, x + 200, 60);
+}
+
+function drawEye(x) {
   noStroke();
-  // eye 1
-  ellipse(200, 200, 200, 200);
-  // eye 2
-  ellipse(500, 200, 200, 200);
+  fill('white');
+  ellipse(x, 200, 200, 200);
+  //pupil
   fill('black');
-  //pupil 1
-  ellipse(200, 200, 50, 50);
-  //pupil 2
-  ellipse(500, 200, 50, 50);
-  // Sunglasses
+  ellipse(x, 200, 50, 50);
+}
+
+function drawSunglasses() {
   fill(143, 75, 3, 200);
   stroke('black');
   strokeWeight(5);
@@ -30,16 +45,15 @@ function draw() {
   line(325, 200, 375, 200);
   line(0, 190, 75, 200);
   line(625, 200, 655, 190);
-  // mouth
+}
+
+function drawMouth() {
   fill('hotpink');
-  //ellipse(345, 400, 550, 50);
   arc(345, 400, 250, 250, 0, PI, CHORD);
-  // nose
+}
+
+function drawNose() {
   stroke('black');
   noFill();
   triangle(350, 250, 300, 350, 400, 350);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
